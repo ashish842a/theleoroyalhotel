3f422253740455453c0ee21ff974dcb494b7b339
Ashish Kumar <ashishkumar0842a@gmail.com>
2026-02-15 12:19:25 +0530

    change in this next.config.ts file for get url from .env file

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,28 +4,29 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
+        protocol: "https",
+        hostname: "via.placeholder.com",
       },
       {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
       {
-        protocol: 'http',
-        hostname: 'example.com',
+        protocol: "http",
+        hostname: "example.com",
       },
     ],
   },
